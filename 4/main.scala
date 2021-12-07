6bf1262354afbbf6a1397e4f9c4666671280579e
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-12-07 00:32:04 +0000

    Add Boilerplate code for CW4

--- 4/main.scala
+++ 4/main.scala
@@ -1,14 +1,14 @@
-
-// A simple lexer inspired by work of Sulzmann & Lu
-//==================================================
-
 /* 
  * Author: Ayan Ahmad
  * K-Number: K19002255
  * Email: ayan.ahmad@kcl.ac.uk
 */
 
-// Copied Coursework 1 and 2 code
+
+// scalac test.scala
+// javap -c Foo.class
+
+// Copied Coursework 1, 2 and 3 code
 
 /* Class Definitions */
 abstract class Rexp
@@ -451,18 +451,6 @@ lazy val BExp: Parser[List[Token], BExp] =
    (T_KWD("false").map[BExp]{ _ => False }) ||
    (T_LPAREN_N ~ BExp ~ T_RPAREN_N).map[BExp]{ case _ ~ x ~ _ => x }
 
-
-// a single statement 
-/* we need 4 types of writes here
- write id
- write "string"
- write (id)
- write ("string")
-
- and 2 types of reads
- read n
- read (n)
-*/ 
 lazy val Stmt: Parser[List[Token], Stmt] =
   ((T_KWD("skip").map[Stmt]{_ => Skip }) ||
    (IdParserToken ~ T_OP(":=") ~ AExp).map[Stmt]{ case x ~ _ ~ z => Assign(x, z) } ||
@@ -555,39 +543,225 @@ def time_needed[T](code: => T) = {
 
 }
 
+// Coursework 3 Code
+
+// additional statements for Arrays 
+case class ArrayDef(s: String, n: Int) extends Stmt            // array definition
+case class AssignA(s: String, a1: AExp, a2: AExp) extends Stmt // arr[exp1] := exp2
+case class Ref(s: String, a: AExp) extends AExp
+
+
+// compiler headers needed for the JVM
+//
+// - contains a main method and a method for writing out an integer
+//
+// - the stack and locals are hard-coded
+//
+
+val beginning = """
+.class public XXX.XXX
+.super java/lang/Object
+
+.method public static write(I)V 
+    .limit locals 1 
+    .limit stack 2 
+    getstatic java/lang/System/out Ljava/io/PrintStream; 
+    iload 0
+    invokevirtual java/io/PrintStream/print(I)V
+    return 
+.end method
+
+.method public static main([Ljava/lang/String;)V
+   .limit locals 200
+   .limit stack 200
+
+; COMPILED CODE STARTS   
+
+"""
+
+val ending = """
+; COMPILED CODE ENDS
+   return
+
+.end method
+"""
+
+
+
+// for generating new labels
+var counter = -1
+
+def Fresh(x: String) = {
+  counter += 1
+  x ++ "_" ++ counter.toString()
+}
+
+// environments for variables and indices
+type Env = Map[String, Int]
+
+// convenient string interpolations 
+// for generating instructions and labels
+
+implicit def sring_inters(sc: StringContext) = new {
+    def i(args: Any*): String = "   " ++ sc.s(args:_*) ++ "\n"
+    def l(args: Any*): String = sc.s(args:_*) ++ ":\n"
+}
+
+def compile_op(op: String) = op match {
+  case "+" => i"iadd"
+  case "-" => i"isub"
+  case "*" => i"imul"
+}
+
+// arithmetic expression compilation
+def compile_aexp(a: AExp, env : Env) : String = a match {
+  case Num(i) => i"ldc $i"
+  case Var(s) => i"iload ${env(s)}"
+  case Aop(op, a1, a2) => 
+    compile_aexp(a1, env) ++ compile_aexp(a2, env) ++ compile_op(op)
+  case Ref(s, a) =>
+    i"aload ${env(s)}" ++ compile_aexp(a, env) ++  i"iaload"
+}
+
+// boolean expression compilation
+def compile_bexp(b: BExp, env : Env, jmp: String) : String = b match {
+  case True => ""
+  case False => i"goto $jmp"
+  case Bop("==", a1, a2) => 
+    compile_aexp(a1, env) ++ compile_aexp(a2, env) ++ i"if_icmpne $jmp"
+  case Bop("!=", a1, a2) => 
+    compile_aexp(a1, env) ++ compile_aexp(a2, env) ++ i"if_icmpeq $jmp"
+  case Bop("<", a1, a2) => 
+    compile_aexp(a1, env) ++ compile_aexp(a2, env) ++ i"if_icmpge $jmp"
+}
+
+// statement compilation
+def compile_stmt(s: Stmt, env: Env) : (String, Env) = s match {
+  case Skip => ("", env)
+  case Assign(x, a) => {
+     val index = env.getOrElse(x, env.keys.size)
+    (compile_aexp(a, env) ++ i"istore $index \t\t; $x", env + (x -> index)) 
+  } 
+  case If(b, bl1, bl2) => {
+    val if_else = Fresh("If_else")
+    val if_end = Fresh("If_end")
+    val (instrs1, env1) = compile_block(bl1, env)
+    val (instrs2, env2) = compile_block(bl2, env1)
+    (compile_bexp(b, env, if_else) ++
+     instrs1 ++
+     i"goto $if_end" ++
+     l"$if_else" ++
+     instrs2 ++
+     l"$if_end", env2)
+  }
+  case While(b, bl) => {
+    val loop_begin = Fresh("Loop_begin")
+    val loop_end = Fresh("Loop_end")
+    val (instrs1, env1) = compile_block(bl, env)
+    (l"$loop_begin" ++
+     compile_bexp(b, env, loop_end) ++
+     instrs1 ++
+     i"goto $loop_begin" ++
+     l"$loop_end", env1)
+  }
+  case Write(x) => 
+    (i"iload ${env(x)} \t\t; $x" ++ 
+     i"invokestatic XXX/XXX/write(I)V", env)
+  case ArrayDef(s: String, n: Int) => {
+    val index = if (env.isDefinedAt(s)) throw new Exception("array def error") else 
+                    env.keys.size
+    (i"ldc $n" ++
+     i"newarray int" ++
+     i"astore $index", env + (s -> index))
+  }
+  case AssignA(s, a1, a2) => {
+    val index = if (env.isDefinedAt(s)) env(s) else 
+                    throw new Exception("array not defined")
+    (i"aload ${env(s)}" ++
+     compile_aexp(a1, env) ++
+     compile_aexp(a2, env) ++
+     i"iastore", env)
+  } 
+}
+
+// compile a block (i.e. list of statements)
+def compile_block(bl: Block, env: Env) : (String, Env) = bl match {
+  case Nil => ("", env)
+  case s::bl => {
+    val (instrs1, env1) = compile_stmt(s, env)
+    val (instrs2, env2) = compile_block(bl, env1)
+    (instrs1 ++ instrs2, env2)
+  }
+}
+
+
+// main compile function for blocks (adds headers and proper JVM names)
+def compile(bl: Block, class_name: String) : String = {
+  val instructions = compile_block(bl, Map())._1
+  (beginning ++ instructions ++ ending).replace("XXX", class_name)
+}
+
+
+
+// contrived example involving arrays
+val array_test = 
+  List(ArrayDef("a", 10),               // array a[10]
+       ArrayDef("b", 2),                // array b[2]
+       AssignA("a", Num(0), Num(10)),   // a[0] := 10
+       Assign("x", Ref("a", Num(0))),   // x := a[0]
+       Write("x"),            
+       AssignA("b", Num(1), Num(5)),    // b[1] := 5
+       Assign("x", Ref("b", Num(1))),   // x := b[1] 
+       Write("x"))                     
+
+
+// prints out the JVM-assembly instructions for fib above
+//
+//    println(compile(array_test, "arr"))
+//
+// can be assembled by hand with 
+//
+//    java -jar jasmin.jar arr.j
+//
+// and run with
+//
+//    java arr/arr
+
+// automating the above
+import ammonite.ops._
+
+def compile_to_file(bl: Block, class_name: String) : Unit = 
+  write.over(pwd / s"$class_name.j", compile(bl, class_name))  
+
+def compile_and_run(bl: Block, class_name: String) : Unit = {
+  println(s"Start of compilation")
+  compile_to_file(bl, class_name)
+  println(s"generated $class_name.j file")
+  os.proc("java", "-jar", "jasmin.jar", s"$class_name.j").call()
+  println(s"generated $class_name.class file ")
+  //println(os.proc("java", s"${class_name}/${class_name}").call().out.text())
+  os.proc("java", s"${class_name}/${class_name}").call(stdout = os.Inherit)
+  println(s"done.")
+}
+
+
+   
+@main def main() = {
+  compile_and_run(array_test, "arr")
+}
+
+
+
+
+// runs with amm2 and amm3
+
+
+
 
 
 @arg(doc = "Question 1 Tests")
 @main
 def q1() = {
-
-    println("_____Q1 Tests______");
-    StrParserToken.parse(List(T_STR("sing"), T_STR("song")))
-    IdParserToken.parse(List(T_ID("K190"),T_KWD("read")))
-    NumParserToken.parse(List(T_NUM(190),T_KWD("read")))
-
-    // 5 * (10)
-    println(AExp.parse_all(List(T_NUM(5), T_OP("*"), T_LPAREN_N, T_NUM(10), T_RPAREN_N)))
-
-    // 8 * (5-5)
-    println(AExp.parse_all(List(T_NUM(8), T_OP("*"), T_LPAREN_N, T_NUM(5), T_OP("-"), T_NUM(5), T_RPAREN_N)))
-
-    // (1>2) || ((5>=6) && ((false || true) && ((1!=2) && (5<=6))))
-    println(BExp.parse_all(List(T_LPAREN_N, T_NUM(1), T_OP(">"), T_NUM(2), 
-    T_RPAREN_N, T_OP("||"), T_LPAREN_N, T_LPAREN_N, T_NUM(5), T_OP(">="), 
-    T_NUM(6), T_RPAREN_N, T_OP("&&"), T_LPAREN_N, T_LPAREN_N, T_NUM(3), 
-    T_OP("<"), T_NUM(4), T_RPAREN_N, T_OP("&&"), T_LPAREN_N, T_LPAREN_N,
-    T_NUM(1), T_OP("!="), T_NUM(2), T_RPAREN_N, T_OP("&&"), T_LPAREN_N, T_NUM(5), 
-    T_OP("<="), T_NUM(6), T_RPAREN_N, T_RPAREN_N, T_RPAREN_N, T_RPAREN_N)))
-
-    // Test skip
-    println(Stmt.parse_all(List(T_KWD("skip"))))
-
-    // Test x := x + 1
-    println(Stmt.parse_all(List(T_ID("x"), T_OP(":="), T_ID("x"), T_OP("+"), T_NUM(10))))
-
-    // Test while (a < 5) do { b := b + 1; a := a + 1 }
-    println(Stmt.parse_all(List(T_KWD("while"), T_LPAREN_N, T_ID("a"), T_OP("<"), T_NUM(5), T_RPAREN_N, T_KWD("do"), T_LPAREN_C, T_ID("b"), T_OP(":="), T_ID("b"), T_OP("+"), T_NUM(1), T_SEMI, T_ID("a"), T_OP(":="), T_ID("a"), T_OP("+"), T_NUM(1), T_RPAREN_C)))
 
 }
 
@@ -662,16 +836,6 @@ def q2() = {
     bnd := bnd + 1
     }"""
 
-    println("__________Fib Parse Tree__________")
-    println(Stmts.parse_all(tokenise(fig1)))
-    println("__________Loops Parse Tree__________")
-    println(Stmts.parse_all(tokenise(fig2)))
-    println("__________Primes Parse Tree__________")
-    println(Stmts.parse_all(tokenise(fig3)))
-    println("__________Collatz Parse Tree__________")
-    println(Stmts.parse_all(tokenise(fig4)))
-    println("__________Additional Parse Tree__________")
-    println(Stmts.parse_all(tokenise("if (a < b) then skip else a := a * b + 1")))
 
 }
 
@@ -773,27 +937,6 @@ def q3() = {
         x := x - 1
     }"""
 
-    // Test Without Time
-    // println(eval(Stmts.parse_all(tokenise(fig1)).head))
-    // println(eval(Stmts.parse_all(tokenise(fig2)).head))
-    // println(eval(Stmts.parse_all(tokenise(fig3)).head))
-    // println(eval(Stmts.parse_all(tokenise(fig4)).head))
-
-
-    //Test with Time
-    time_needed(eval(Stmts.parse_all(tokenise(fig1)).head))
-    time_needed(eval(Stmts.parse_all(tokenise(fig3)).head))
-    time_needed(eval(Stmts.parse_all(tokenise(fig4)).head))
-
-    println("Loop Program - start: 100")
-    time_needed(eval(Stmts.parse_all(tokenise(fig2)).head))
-
-
-    println("Loop Program - start: 500")
-    time_needed(eval(Stmts.parse_all(tokenise(fig2_500)).head))
-
-    println("Loop Program - start: 800")
-    time_needed(eval(Stmts.parse_all(tokenise(fig2_800)).head))
 
 
 }
